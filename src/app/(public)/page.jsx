5ae71db83ec85fede6e5154e14157656c814361c
jhersara <linerslink@gmail.com>
2025-11-07 19:25:39 -0500

    Implementacion de una nueva barra de navegacion

--- src/app/(public)/page.jsx
+++ src/app/(public)/page.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { RiMedalLine} from 'react-icons/ri'
+import { RiMedalLine, RiArrowRightLine} from 'react-icons/ri'
 import '@/styles/pages/page.css'
 import Image from 'next/image'
 
@@ -64,7 +64,7 @@ export default function HomePage() {
           <h1>Invierte con <span className="strong">Confianza</span><br />y Transparencia</h1>
           <p className="text">Accede  a oportunidades de invercion profecionales con reportes diarios, seguridad <br />institucional y rendimientos superiores al mercado.</p>
           <div className="btn_content">
-            <a href="/" className="btn actived">Comienza a invertir</a>
+            <a href="/" className="btn actived">Comienza a invertir <RiArrowRightLine /></a>
             <a href="/" className="btn on_actived">Conoce mas</a>
           </div>
         </div>
